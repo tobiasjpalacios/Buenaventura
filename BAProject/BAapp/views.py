62927053c1036940cb9c57ba4cf71554bc22cd62
Nicolas Bazan <nicolas.bazan@mi.unc.edu.ar>
2020-10-24 22:00:28 -0300

    signup/in sin terminar

--- BAProject/BAapp/views.py
+++ BAProject/BAapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from .forms import *
 from .models import *
+
 
 def landing_page(request):
 	return render(request,'Principal.html')
@@ -14,3 +17,29 @@ def crear_proveedor(request):
     else:
         form = ProveedorForm()
     return render(request,'crear_proveedor.html',{'form': form})
+
+def crear_usuario(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form':form})
+
+def login_usuario(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+	    login(request, user)
+	    return redirect('')
+	else:
+	    messages.error(request, 'Password or Username incorrect, please try again.')
+	return render(request, '')
