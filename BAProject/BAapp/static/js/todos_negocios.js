f4e4854b6850d89b6b6d3441aeb6a9bf9da62165
juampiludu <juanzakka@gmail.com>
2021-12-13 22:07:33 -0300

    agregue cambios charlados  en filtro de negocios

--- BAProject/BAapp/static/js/todos_negocios.js
+++ BAProject/BAapp/static/js/todos_negocios.js
@@ -1,6 +1,32 @@
-  $(document).ready(function(){
-    $('select').formSelect();
+$(document).ready(function(){
+  $('select').formSelect();
+  $('.tooltipped').tooltip();
+  $('.scrollspy').scrollSpy();
+  $('.collapsible').collapsible();
+  $('.fixed-action-btn').floatingActionButton();
+  $('.modal').modal();
+  $('.logis').hide();
+  $('#ocultar').hide();
+  $('#campo').hide();
+
+  $('#mostrar').click(function(){
+    $('#ocultar').show();
+    $('.logistica').hide();
+    $('#mostrar').hide();
+    $('.logis').show();
+
   });
+
+  $('#ocultar').click(function(){
+      $('.logis').hide();
+      $('.logistica').show();
+      $('#ocultar').hide();
+      $('#mostrar').show();
+  });  
+
+  $('input#input_text, textarea#textarea2').characterCounter();
+
+});
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.datepicker');
@@ -65,10 +91,6 @@
     }
     var instances = M.Datepicker.init(elems, options);
   });
-
-  $(document).ready(function() {
-    $('input#input_text, textarea#textarea2').characterCounter();
-  });
       
   function searchBuscadorNegocio(){ 
       var value = $("#inputBuscadorNegocio").val().toLowerCase();
@@ -87,34 +109,34 @@
   //   }
   // }
 
-  $(document).ready(function(){
-  $('.scrollspy').scrollSpy();
-  $('.collapsible').collapsible();
-  $('.fixed-action-btn').floatingActionButton();
-  $('.modal').modal();
-  $('.logis').hide();
-  $('#ocultar').hide();
-  $('#campo').hide();
-
-  $('#mostrar').click(function(){
-    $('#ocultar').show();
-    $('.logistica').hide();
-    $('#mostrar').hide();
-    $('.logis').show();
-
- });
-
- $('#ocultar').click(function(){
-    $('.logis').hide();
-    $('.logistica').show();
-    $('#ocultar').hide();
-    $('#mostrar').show();
-
- });   
-});
-
-// estas "funciones" arreglan los problemas relacionos con los multiple options de los filtros.
+// esta funcion arregla los problemas relacionos con los multiple options de los filtros.
 // todavia hay un "error", que cuando se actualiza un selected, se cierran las opciones.
+
+function multipleOptionFirstItem(id, select, varId, todos, counter) {
+  if (varId.length > 1 && varId[0] == "todos") {
+    if (counter < 1) {
+      todos.prop('selected', false);
+      select.formSelect();
+      counter++
+    }
+    else {
+      $('#'+id+' > option').each(function() {
+        if ($(this).val() != "todos") {
+          $(this).prop('selected', false);
+        }
+        select.formSelect();
+        counter=0
+      })
+    }
+  }
+
+  if (varId.length == 0) {
+    todos.prop('selected', 'selected');
+    select.formSelect();
+  }
+
+  return counter;
+}
 
 var i = 0
 $('#selectVendedores').on('change', function(e) {
@@ -123,27 +145,8 @@ $('#selectVendedores').on('change', function(e) {
   var vendedores = select.val();
   var todos = select.find('option:eq(0)');
 
-  if (vendedores.length > 1 && vendedores[0] == "todos") {
-    if (i < 1) {
-      todos.prop('selected', false);
-      select.formSelect();
-      i++
-    }
-    else {
-      $('#selectVendedores > option').each(function() {
-        if ($(this).val() != "todos") {
-          $(this).prop('selected', false);
-        }
-        select.formSelect();
-        i=0
-      })
-    }
-  }
-
-  if (vendedores.length == 0) {
-    todos.prop('selected', 'selected');
-    select.formSelect();
-  }
+  var fun = multipleOptionFirstItem('selectVendedores', select, vendedores, todos, i);
+  i = fun;
 
 });
 
@@ -154,26 +157,76 @@ $('#selectEstados').on('change', function(e) {
   var estados = select.val();
   var todos = select.find('option:eq(0)');
 
-  if (estados.length > 1 && estados[0] == "todos") {
-    if (o < 1) {
-      todos.prop('selected', false);
-      select.formSelect();
-      o++
-    }
-    else {
-      $('#selectEstados > option').each(function() {
-        if ($(this).val() != "todos") {
-          $(this).prop('selected', false);
-        }
-        select.formSelect();
-        o=0
-      })
-    }
-  }
-
-  if (estados.length == 0) {
-    todos.prop('selected', 'selected');
-    select.formSelect();
-  }
+  var fun = multipleOptionFirstItem('selectEstados', select, estados, todos, o);
+  o = fun;
 
 });
+
+// scroll con filtro y head fijos
+
+var $a = $("#fixedForScroll");
+
+// table
+var tableOffset = $("#tableNegocios").offset().top;
+var $header = $("#tableNegocios > thead");
+var $fixedHeader = $("#headerFixed").append($header.clone());
+var headerWidth = $header.width()
+
+// filter
+var filterOffset = $("#navFiltro > div").offset().top;
+var $fixedFilter = $("#filtroFixed");
+var filterHeight = $("#navFiltro").height();
+var filterWidth = $("#navFiltro").width();
+
+// nav
+var nav = $("nav");
+var navHeight = nav.height();
+
+$a.css({
+  top:navHeight+"px"
+});
+$fixedFilter.css({
+  width: filterWidth+"px"
+});
+$fixedHeader.css({
+  width: filterWidth+"px"
+});
+
+$(window).bind("scroll", function() {
+  var offset = $(this).scrollTop();
+
+  if (offset >= tableOffset && $fixedHeader.is(":hidden")) {
+    $filterClone = $("#navFiltro > div").detach();
+    $fixedFilter = $("#filtroFixed").append($filterClone);
+
+    $.each($header.find('tr > th'), function(ind, val) {
+      var original_width = $(val).width();
+      $($fixedHeader.find('tr > th')[ind]).width(original_width);
+    });
+
+    $("#fixedForScroll").slideDown("fast");
+  }
+  else if (offset < tableOffset) {
+    var filterClone = $("#filtroFixed > div").detach();
+    $("#navFiltro").append(filterClone);
+
+    $("#fixedForScroll").slideUp("fast");
+  }
+});
+
+// limpia filtros
+
+function limpiarFiltros() {
+  var $idDeNeg = $("#idDeNeg"); 
+  var $fechaDesde = $("#fechaDesde");
+  var $fechaHasta = $("#fechaHasta");
+
+  $idDeNeg.val("");
+  $fechaDesde.val("");
+  $fechaHasta.val("");
+
+  $("#selectVendedores option[value='todos']").prop('selected', true).change();
+  $("#selectEstados option[value='todos']").prop('selected', true).change();
+  $("#selectTipo option[value='todos']").prop('selected', true).change();
+  $("#selectTipo").formSelect();
+}
