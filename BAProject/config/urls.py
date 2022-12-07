3d4f6d0aa5382fdcd6959a72d38d7a7452370181
wilitp <guilledeipola@gmail.com>
2022-12-07 20:48:39 -0300

    Django maneja los staticfiles en desarrollo

--- BAProject/config/urls.py
+++ BAProject/config/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('', include('BAapp.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL ,document_root=settings.STATIC_ROOT)
