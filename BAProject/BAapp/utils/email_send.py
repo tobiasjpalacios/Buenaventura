b94bfe4808e3ff8c33237e61edab73fc352d9c17
juampiludu <juanzakka@gmail.com>
2023-08-24 00:26:00 -0300

    quick fix se quitó la libreria requests

--- BAProject/BAapp/utils/email_send.py
+++ BAProject/BAapp/utils/email_send.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
-import requests
 
 def email_send(
         subject_email : str, 
